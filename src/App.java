a01079908b50763babc0cd83064efec081f8ba2d
unknown <HugoAndres4>
2021-06-29 21:02:17 -0500

    Second class Java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        // Tipos de Datos y Operadores
+        int resultado = 15 + 32;
+        String res = "Hola";
+        double resultado2 = 25 / 3;
+        int resultado3 = 25 - 3;
+        float resultado4 = 58 * 21;
+        int resultado5 = 21 % 9;
+
+        // Operadores Unarios (permiten cambiar el valor de una variable)
+        resultado++;  //sumarle 1 resultado = resultado + 1
+        resultado--;  //restarle 1 resultado = resultado - 1
+        resultado = -resultado;
+        resultado = +resultado;
+
+        // Operadores Logícos
     }
-}
+ }
